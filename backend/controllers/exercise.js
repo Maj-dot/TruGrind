8ce824f567715976e3c89dd3f312713bf8124a93
BHerbert <madammaj418@gmail.com>
2024-09-16 00:39:59 -0400

    Create index functionality

--- backend/controllers/exercise.js
+++ backend/controllers/exercise.js
@@ -1,19 +1,32 @@
-const Exercise = require('../models/exercise');
+const Exercise = require("../models/exercise");
 
 module.exports = {
-    create,
+  create,
+  index,
 };
 
 // Create exercise
 async function create(req, res) {
-    try {
-        const exerciseData = {
-            ...req.body,
-            user:req.user._id
-        };
+  try {
+    const exerciseData = {
+      ...req.body,
+      user: req.user._id,
+    };
+    const exercise = await Exercise.create(exerciseData);
+    res.status(201).json(exercise);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
 
-        const exercise = await Exercise.create(exerciseData);
-        res.status(201).json(exercise);
+// Index exercises
+async function index(req, res) {
+    try {
+        const exercises = await Exercise.find({})
+            .populate("user")
+            .sort({createdAt: "desc"});
+            console.log(exercises);
+        res.status(200).json(exercises);    
     } catch (err) {
         res.status(400).json(err);
     }
--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const exerciseCtrl = require('../controllers/exercise');
 
+// All paths start with '/api/exercise'
+
 // POST /api/exercise
 router.post('/', exerciseCtrl.create);
+
+// GET /api/exercise
+router.get('/', exerciseCtrl.index);
 
 module.exports = router; 
